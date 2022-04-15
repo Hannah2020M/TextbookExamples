ff561a8b098ccb26babdaba6049d19f28bfe86d5
Hannah2020M <coder2020aug@gmail.com>
2022-04-15 15:51:10 -0500

    used the kEYWORD this

--- HelloWorldClass.java
+++ HelloWorldClass.java
@@ -1,7 +1,10 @@
 package TextbookExamples;
 import java.util.Scanner;
+
 public class HelloWorldClass {
+    public String message = "Hello World";
     public void sayHello(String userMessage) {
          System.out.println(userMessage);
+        System.out.println(this.message);
     }
 }
